2e9d3c1514014701340c244759d7ab0c330b4640
Mr. Kavinda Dilshan <kdtechniques@gmail.com>
2024-11-12 16:57:41 +0530

    [Featue] add `ContentView` for testing purposes

--- CountdownFlipperSwiftUIUITesterApp/ContentView.swift
+++ CountdownFlipperSwiftUIUITesterApp/ContentView.swift
@@ -6,16 +6,23 @@
 //
 
 import SwiftUI
+import CountdownFlipperSwiftUI
 
 struct ContentView: View {
+    @State private var counter: Int = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 50) {
+            TimeView()
+            
+            CountdownFlipperView(flipperType: .flipFromTop, counter: $counter, fontSize: 200)
+            
+            Button("Generate Random Number: \(counter)") {
+                counter = .random(in: 0...9)
+            }
+            .buttonStyle(.bordered)
+            .controlSize(.large)
         }
-        .padding()
     }
 }
 
