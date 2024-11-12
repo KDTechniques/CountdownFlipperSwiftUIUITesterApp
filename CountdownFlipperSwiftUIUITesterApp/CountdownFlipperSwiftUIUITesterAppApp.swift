aaf170931f1d66630ef10f994600684c468dd169
Mr. Kavinda Dilshan <kdtechniques@gmail.com>
2024-11-12 13:08:27 +0530

    Initial Commit

---
+++ CountdownFlipperSwiftUIUITesterApp/CountdownFlipperSwiftUIUITesterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownFlipperSwiftUIUITesterAppApp.swift
+//  CountdownFlipperSwiftUIUITesterApp
+//
+//  Created by Kavinda Dilshan on 2024-11-12.
+//
+
+import SwiftUI
+
+@main
+struct CountdownFlipperSwiftUIUITesterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
